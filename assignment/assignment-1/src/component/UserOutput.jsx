537b531aae2871ecff6f515b28dbbaf830c495b6
mones <mones.cse@gmail.com>
2021-03-31 16:44:04 +0600

    added inline style to assignment-1

--- assignment/assignment-1/src/component/UserOutput.jsx
+++ assignment/assignment-1/src/component/UserOutput.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 const UserOutput = (props) =>{
+    const style ={
+        border:"1px solid #aaa",
+        margin:"10px 0px",
+        boxShadow:"3px 3px 10px #999",
+        padding:"15px"
+    }
     return(
-        <div>
+        <div style={style}>
             <p>Hello {props.userName}</p>
             <p>Welcome</p>
         </div>
